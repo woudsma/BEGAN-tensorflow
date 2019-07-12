38493023bcb23c2b4be910a0c8d26a71943084d5
Tjerk Woudsma <tjerk@studiomoniker.com>
2019-07-12 20:00:02 +0200

    update

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ net_arg.add_argument('--z_num', type=int, default=64, choices=[64, 128])
 data_arg = add_argument_group('Data')
 data_arg.add_argument('--dataset', type=str, default='CelebA')
 data_arg.add_argument('--split', type=str, default='train')
-data_arg.add_argument('--batch_size', type=int, default=4)
+data_arg.add_argument('--batch_size', type=int, default=2)
 data_arg.add_argument('--grayscale', type=str2bool, default=True)
 data_arg.add_argument('--num_worker', type=int, default=4)
 
